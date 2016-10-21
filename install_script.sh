bfea2f7ba5670995aa7691a76ad9b7abbd268ac1
Louishancquart <hancquartlouis@gmail.com>
2016-10-21 11:21:53 +0200

    add install script

---
+++ install_script.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y gradle
+sudo apt install -y gimp
+sudo apt install -y chromium-browser
+sudo apt install -y docker
+sudo apt install -y apparix 
+sudo apt install -y zeitgeist 
+sudo apt install -y geary 
+sudo apt-get install unrar
+wget https://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
+sudo dpkg -i skype-ubuntu-precise_4.3.0.37-1_i386.deb
+sudo rm -rf skype-ubuntu-precise_4.3.0.37-1_i386.deb
+#wget https://nodejs.org/dist/v4.4.7/node-v4.4.7-linux-x64.tar.xz
+#mkdir -p ~/Download/apps/nodejs
+#sudo tar xpvf node-v4.4.7-linux-x64.tar.xz ~/Download/apps/nodejs
+#chown -R m node-v4.4.7-linux-x64.tar.xz 
+#sudo ln -s ~/Downloads/apps/nodejs/bin/node /usr/bin/node
+#sudo ln -s ~/Downloads/apps/nodejs/bin/npm /usr/bin/npm
+
+echo ""
+echo "************************"
+echo "everything is in :) "
